be7a179ca302ece26a11958e9da370f8da64edc2
Narendra Syahrasyad <nanda.s@hey.com>
2020-08-21 20:22:36 -0700

    feat: add project styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import clsx from 'clsx'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
 
+import Tapestry from '../components/Tapestry'
 import styles from '../css/Home.module.scss'
 import useMeta from '../hooks/useMeta'
 import useProjects, { Project } from '../hooks/useProjects'
@@ -34,9 +37,36 @@ export default function Home() {
 }
 
 function ProjectCard({ frontmatter }: Project) {
+  const components: Record<string, () => JSX.Element> = {
+    Tapestry,
+  }
+  const Component = components[frontmatter.title]
   return (
-    <section className="p-4 h-64 rounded-md bg-gray-200 mb-4 hover:bg-blue-200 flex flex-col-reverse items-start">
-      <h1 className="text-lg font-semibold">{frontmatter.title}</h1>
+    <section
+      className={clsx(
+        'p-6 rounded-md bg-gray-200 mb-4 hover:bg-blue-200 flex flex-col-reverse items-start relative',
+        styles.project
+      )}
+    >
+      <header>
+        <h1 className="text-lg font-semibold">{frontmatter.title}</h1>
+        <p className="text-xs text-gray-600 font-light">
+          {frontmatter.tech.map(name => (
+            <span className="mr-1" key={name}>
+              {name}
+            </span>
+          ))}
+        </p>
+      </header>
+      {Component && <Component />}
+      <button
+        className={clsx(
+          'w-10 h-10 bg-gray-100 absolute rounded-full text-gray-600',
+          styles.project_button
+        )}
+      >
+        <FontAwesomeIcon icon={faArrowRight} />
+      </button>
     </section>
   )
 }
